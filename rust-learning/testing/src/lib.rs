e4f17f75bffa437375cf059c2f2fcd96ff21765b
Tarkin25 <severin.weigold@edu.tbz.ch>
2021-05-03 14:47:39 +0200

    rust learning - testing

---
+++ rust-learning/testing/src/lib.rs
@@ -0,0 +1,85 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+pub struct Guess {
+    value: i32
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Self {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess {
+            value
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn internal_adder_works() {
+        let result = internal_adder(2, 3);
+
+        assert_eq!(5, result);
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7
+        };
+
+        let smaller = Rectangle {
+            width: 5,
+            height: 1
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    #[should_panic]
+    fn guess_greater_than_100() {
+        Guess::new(101);
+    }
+
+}
