3a4177d7e14752bee2b4c307ba23c4ab5071c8ea
Tarkin25 <severin.weigold@edu.tbz.ch>
2021-08-23 08:43:46 +0200

    spring-kafka listen for messages

--- spring-kafka/src/main/java/com/example/springkafka/SpringKafkaApplication.java
+++ spring-kafka/src/main/java/com/example/springkafka/SpringKafkaApplication.java
@@ -1,12 +1,16 @@
 package com.example.springkafka;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.KafkaHandler;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
+@Slf4j
 public class SpringKafkaApplication {
 
     public static void main(String[] args) {
@@ -18,6 +22,11 @@ public class SpringKafkaApplication {
         return args -> {
           template.send("test_topic", "Hello World!");
         };
+    }
+
+    @KafkaListener(topics = "test_topic")
+    public void handleMessage(String message) {
+        log.info("Received message: {}", message);
     }
 
 }
